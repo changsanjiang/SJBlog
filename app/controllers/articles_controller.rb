145533d62a68a19c6065c8b1122fe09f7a47d2e1
SanJiang <changsanjiang@gmail.com>
2017-10-31 20:16:50 +0800

    blog C R

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,39 @@
+class ArticlesController < ApplicationController
+    
+    def index
+        @articles = Article.all
+    end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    def new
+        # 重新赋值, 清空数据
+        @article = Article.new
+    end
+    
+    def create
+        # 要求 参数 article 在控制器中使用. 允许的字段 title, text
+        @article = Article.new(article_params)
+        
+        if @article.save then
+            # redirect 重定向. 将页面重定向到 show 动作.
+            redirect_to @article
+        else
+            render 'new'
+        end
+        
+        
+#        params 请求的参数集合.
+#        例如: http://www.example.com/?username=ruby&&email=ruby@email.com
+#        其中: params[:username] 的值是 ruby
+#        render plain: params[:article].inspect
+    end
+    
+    private
+    
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
+end
---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ApplicationRecord
+    # 用于确保每篇文章都有标题, 并且标题长度不少于 5 个字符.
+    validates :title, presence: true, length:{ minimum: 5 }
+end
