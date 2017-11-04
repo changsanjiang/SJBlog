bb5166d48759975e77de45d61f15257ca2a276f1
SanJiang <changsanjiang@gmail.com>
2017-11-04 18:02:10 +0800

    添加删除评论

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-    has_many :comments
+    has_many :comments, dependent: :destroy
     # 用于确保每篇文章都有标题, 并且标题长度不少于 5 个字符.
     validates :title, presence: true, length:{ minimum: 5 }
 end
