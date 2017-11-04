2c4618b4426e11798cbf772940e5a655cf1915ae
SanJiang <changsanjiang@gmail.com>
2017-11-04 17:47:59 +0800

    添加和显示评论

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,9 @@
 class ArticlesController < ApplicationController
-    
+
     def index
         @articles = Article.all
     end
-    
+
     def show
         @article = Article.find(params[:id])
     end
@@ -12,34 +12,34 @@ class ArticlesController < ApplicationController
         # 重新赋值, 清空数据
         @article = Article.new
     end
-    
+
     def edit
         @article = Article.find(params[:id])
     end
-    
+
     def create
         # 要求 参数 article 在控制器中使用. 允许的字段 title, text
         @article = Article.new(article_params)
-        
+
         if @article.save then
             # redirect 重定向. 将页面重定向到 show 动作.
             redirect_to @article
         else
             render 'new'
         end
-        
-        
+
+
 #        params 请求的参数集合.
 #        例如: http://www.example.com/?username=ruby&&email=ruby@email.com
 #        其中: params[:username] 的值是 ruby
 #        render plain: params[:article].inspect
     end
-    
+
     # update 动作用于更新已有记录, 他接受一个散列作为参数, 散列中包含想要更新的属性.
     # 如果更新文章时发生错误, 就需要把表单再次显示给用户
     def update
         @article = Article.find(params[:id])
-        
+
         if @article.update(article_params)
             # redirect 重定向. 将页面重定向到 show 动作.
             redirect_to @article
@@ -48,16 +48,16 @@ class ArticlesController < ApplicationController
             render 'edit'
         end
     end
-    
+
     def destroy
         @article = Article.find(params[:id])
         @article.destroy
-        
+
         redirect_to articles_path
     end
-    
+
     private
-    
+
     def article_params
         params.require(:article).permit(:title, :text)
     end
